64b41ed5200626dbad640e110d9c1e56a3209e27
Gavin Woodruff <woodruffgavin13@gmail.com>
2023-11-27 12:11:14 -0600

    Fixed Title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Appointment System Path</title>
+  <title>Appointment System Page</title>
   <link href="styles.css" rel="stylesheet" />
 </head>
 
